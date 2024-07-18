2c0342882abaf428fd9349f90e2d9c9984d109b4
MabelZar <mabel.zar.esp@gmail.com>
2024-07-18 16:37:06 +0200

    completed

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,24 @@
-function add(){
-    //Escribir tu código que aumenta
+let num = 0;
+
+function add() {
+  //Escribir tu código que aumenta
+  num++;
+  document.querySelector("#number").innerHTML = num;
 }
 
-function reset(){
-    //Escribir tu código que hace un reset a 0
+function reset() {
+  //Escribir tu código que hace un reset a 0
+  num = 0;
+  document.querySelector("#number").innerHTML = num;
 }
 
-function decrease(){
-    //Escribir tu código que resta
-};
+function decrease() {
+  //Escribir tu código que resta
+  if(num>0){
+    num--;
+  }
+  document.querySelector("#number").innerHTML = num;
+  
+}
 
-export {add, reset, decrease}
+export { add, reset, decrease };
--- src/js/events.js
+++ src/js/events.js
@@ -1,4 +1,6 @@
 import {add, reset, decrease} from './counter.js'
-
+document.getElementById('add').addEventListener('click',add)
+document.getElementById('decrease').addEventListener('click',decrease)
+document.getElementById('reset').addEventListener('click',reset)
 
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
